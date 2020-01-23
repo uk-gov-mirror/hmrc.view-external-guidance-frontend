dd6505a82b6fa8f9b0ea862f541acbb24523b1fd
R Woollett <55831974+woollett-r@users.noreply.github.com>
2020-01-23 16:00:44 +0000

    Implement QuestionStanza and supprting unit tests

---
+++ test/models/ocelot/stanzas/QuestionStanzaSpec.scala
@@ -0,0 +1,108 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models.ocelot.stanzas
+
+import org.scalatest._
+import play.api.libs.json._
+import scala.util.{Failure, Success, Try}
+
+class QuestionStanzaSpec extends WordSpec with MustMatchers {
+
+  val four: Int = 4
+  val five: Int = 5
+
+  val twoAnswersQuestionStanzaJsonInput =
+    """|{
+       | "type" : "QuestionStanza",
+       | "text": 0,
+       | "answers": [ 0, 2 ],
+       | "next": [ "2", "5" ],
+       | "stack": false
+       |}""".stripMargin
+
+  val threeAnswersQuestionStanzaJsonInput =
+    """|{
+       | "type": "QuestionStanza",
+       | "text": 1,
+       | "answers": [ 3, 4, 5 ],
+       | "next": [ "4", "7", "8" ],
+       | "stack": false
+       |}""".stripMargin
+
+  val missingPropertyQuestionStanzaJsonInput =
+    """|{
+       | "type": "QuestionStanza",
+       | "text": 1,
+       | "answers": [ 3, 4, 5 ],
+       | "stack": false
+       |}""".stripMargin
+
+  val incorrectTypeQuestionStanzaJsonInput =
+    """|{
+       | "type": "QuestionStanza",
+       | "text": 1,
+       | "answers": [ 3, 4, 5 ],
+       | "stack": 0
+       |}""".stripMargin
+
+  "reading a valid QuestionStanza with two answers should create an instance of the class QuestionStanza" in {
+
+    val twoAnswersQuestionStanzaJson: JsValue = Json.parse( twoAnswersQuestionStanzaJsonInput )
+
+    val twoAnswersQuestionStanza: QuestionStanza = twoAnswersQuestionStanzaJson.as[QuestionStanza]
+
+    twoAnswersQuestionStanza mustBe QuestionStanza( 0, Seq( 0, 2 ), Seq( "2", "5" ), stack = false )
+  }
+
+  "reading a valid QuestionStanza with three answers should create an instance of the class QuestionStanza" in {
+
+    val threeAnswersQuestionStanzaJson: JsValue = Json.parse( threeAnswersQuestionStanzaJsonInput )
+
+    val threeAnswersQuestionStanza: QuestionStanza = threeAnswersQuestionStanzaJson.as[QuestionStanza]
+
+    threeAnswersQuestionStanza mustBe QuestionStanza( 1, Seq( 3, four, five ), Seq( "4", "7", "8" ), false )
+  }
+
+  "reading a QuestionStanza with a missing property should cause an exception to be raised" in {
+
+    val missingPropertyQuestionStanzaJson: JsValue = Json.parse( missingPropertyQuestionStanzaJsonInput )
+
+    Try
+    {
+      val questionStanza: QuestionStanza = missingPropertyQuestionStanzaJson.as[QuestionStanza]
+    }
+    match {
+      case Success(_) => fail( "An instance of QuestionStanza should not have been created from incomplete Json" )
+      case Failure( failure ) => println( "Failure message for missing property : " + failure.getMessage )
+    }
+  }
+
+  "reading a QuestionStanza with a property of incorrect type should cause an exception to be raised" in {
+
+    val incorrectTypeQuestionStanzaJson: JsValue = Json.parse( incorrectTypeQuestionStanzaJsonInput )
+
+    Try
+    {
+      val questionStanza: QuestionStanza = incorrectTypeQuestionStanzaJson.as[QuestionStanza]
+    }
+    match {
+      case Success(_) => fail( "An instance of QuestionStanza should not have been created from Json with an incorrect property type" )
+      case Failure( failure ) => println( "Failure message for incorrect property type : " + failure.getMessage )
+    }
+  }
+
+}
