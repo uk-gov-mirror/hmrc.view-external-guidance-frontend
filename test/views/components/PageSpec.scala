271b71e2514fc5b3be286db68239b11af5fd16cd
cmrhmrc <55755517+cmrhmrc@users.noreply.github.com>
2020-02-13 14:20:57 +0000

    EG-278, tests for page, paragraph

--- test/views/components/PageSpec.scala
+++ test/views/components/PageSpec.scala
@@ -19,21 +19,26 @@ package views.components
 import org.scalatest.{Matchers, WordSpec}
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
 import play.api.inject.Injector
-import play.api.i18n.{Messages, MessagesApi}
+import play.api.i18n.{Messages, MessagesApi, Lang}
 import play.api.test.FakeRequest
 import play.twirl.api.Html
 import org.jsoup.Jsoup
 import views.html.components.page
 import models.ui.{Page,H1,Paragraph,Text}
+import org.jsoup.nodes.{Document, Element}
 
 
 class PageSpec extends WordSpec with Matchers with GuiceOneAppPerSuite {
 
-  private trait Test {
+  def asDocument(html: Html): Document = Jsoup.parse(html.toString)
+
+  trait Test {
     private def injector: Injector = app.injector
-    private def messagesApi: MessagesApi = injector.instanceOf[MessagesApi]
+    def messagesApi: MessagesApi = injector.instanceOf[MessagesApi]
+    implicit def messages: Messages = messagesApi.preferred(Seq(Lang("en")))
     val fakeRequest = FakeRequest("GET", "/")
-    implicit def messages: Messages = messagesApi.preferred(fakeRequest)
+
+
     val title = Text("Telling HMRC about extra income",
                      "Tudalen Arddangos Yn Adrodd HMRC am incwm ychwanegol")
 
@@ -41,20 +46,42 @@ class PageSpec extends WordSpec with Matchers with GuiceOneAppPerSuite {
                            "Gwiriwch a oes angen i chi ddweud wrth HMRC am arian ychwanegol rydych chi " +
                            "wedi'i wneud trwy werthu nwyddau neu wasanaethau, neu rentu tir neu eiddo.")
 
-    val simplePage =  Page("root",
-                           Seq(
-                             H1(title),
-                             Paragraph(Seq(openingPara), lede = true)
-                           )
-                      )
+    val para = Paragraph(Seq(openingPara))
+    val simplePage =  Page("root", Seq(para, H1(title)))
+  }
 
+  trait WelshTest extends Test {
+    implicit override def messages: Messages = messagesApi.preferred(Seq(Lang("cy")))
   }
 
   "Page component" should {
-    "generate html containing an H1 and a text only paragraph" in new Test {
-      val html: String = page(simplePage).body.trim
 
-      println(html)
+    "generate English html containing an H1 and a text only paragraph" in new Test {
+
+      val doc = asDocument(page(simplePage))
+
+      val paras = doc.getElementsByTag("p")
+
+      paras.size shouldBe 1
+      paras.first.text shouldBe openingPara.english
+
+      val h1s = doc.getElementsByTag("h1")
+      h1s.size shouldBe 1
+      h1s.first.text shouldBe title.english
+    }
+
+    "generate Welsh html containing an H1 and a text only paragraph" in new WelshTest {
+
+      val doc = asDocument(page(simplePage))
+
+      val paras = doc.getElementsByTag("p")
+
+      paras.size shouldBe 1
+      paras.first.text shouldBe openingPara.welsh
+
+      val h1s = doc.getElementsByTag("h1")
+      h1s.size shouldBe 1
+      h1s.first.text shouldBe title.welsh
     }
 
   }
