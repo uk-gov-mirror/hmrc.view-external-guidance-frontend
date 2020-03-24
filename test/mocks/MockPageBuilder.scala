d2656e36bbcc4d53ec89629d628bf93fc8ccc965
R Woollett <55831974+woollett-r@users.noreply.github.com>
2020-03-24 16:38:57 +0000

    Implement mock page builder in guidance service test

---
+++ test/mocks/MockPageBuilder.scala
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package mocks
+
+import models.ocelot.{Page, Process}
+import services.{FlowError, PageBuilder}
+
+import org.scalamock.handlers.CallHandler
+import org.scalamock.scalatest.MockFactory
+
+trait MockPageBuilder extends MockFactory {
+
+  val mockPageBuilder: PageBuilder = mock[PageBuilder]
+
+  object MockPageBuilder {
+
+    def pages( process: Process, start: String = "start" ) : CallHandler[Either[FlowError,Seq[Page]]] = {
+
+      (mockPageBuilder
+        .pages(_: Process, _: String ) )
+        .expects( process, * )
+    }
+  }
+
+}
--- test/services/GuidanceServiceSpec.scala
+++ test/services/GuidanceServiceSpec.scala
@@ -17,22 +17,101 @@
 package services
 
 import base.BaseSpec
-import mocks.{MockGuidanceConnector, MockSessionRepository}
-import models.ocelot.{Process, ProcessJson}
+import mocks.{MockGuidanceConnector, MockPageBuilder, MockSessionRepository}
+import models.ocelot.{Link, Meta, Page, Phrase, Process, ProcessJson}
+import models.ocelot.stanzas._
 import uk.gov.hmrc.http.HeaderCarrier
+import play.api.libs.json._
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
 class GuidanceServiceSpec extends BaseSpec {
 
-  private trait Test extends MockGuidanceConnector with MockSessionRepository with ProcessJson {
+  private trait Test extends MockGuidanceConnector with MockSessionRepository with MockPageBuilder with ProcessJson {
     implicit val headerCarrier: HeaderCarrier = HeaderCarrier()
-    val process: Process = prototypeJson.as[Process]
+
+    // Define simple Ocelot process
+    private val meta: Meta = Json.parse( prototypeMetaSection ).as[Meta]
+
+    private val page1Id: String = "start"
+    private val page2Id: String = "4"
+    private val page3Id: String = "6"
+
+    private val page1Url: String = "/"
+    private val page2Url: String = "/yes"
+    private val page3Url: String = "/no"
+
+    private val four: Int = 4
+    private val five: Int = 5
+    private val six: Int = 6
+
+    private val page1ValueStanza: ValueStanza = ValueStanza(List(Value(Scalar,"PageUrl",page1Url)),Seq("1"),stack = false)
+    private val page2ValueStanza: ValueStanza = ValueStanza(List(Value(Scalar,"PageUrl",page2Url)),Seq("5"),stack = false)
+    private val page3ValueStanza: ValueStanza = ValueStanza(List(Value(Scalar,"PageUrl",page3Url)),Seq( "10"), stack = false)
+
+
+    private val flow = Map(
+    page1Id -> page1ValueStanza,
+      "1" -> InstructionStanza(0,Seq("2"),None,stack = false),
+      "2" -> QuestionStanza(1,Seq(2,3),Seq(page2Id, page3Id),stack = false),
+
+      page2Id -> page2ValueStanza,
+      "5" -> InstructionStanza(four,Seq("end"), Some(0),stack = false),
+
+      page3Id -> page3ValueStanza,
+      "10" -> InstructionStanza(five,Seq("11"),None,stack = false),
+      "11" -> InstructionStanza(six,Seq("end"),None,stack = false),
+
+      "end" -> EndStanza
+    )
+
+    private val phrases = Vector[Phrase](
+      Phrase(Vector("Hello","Helo")),
+      Phrase(Vector("Would you like a cup of tea?","Hoffech chi gael paned?")),
+      Phrase(Vector("Yes please","os gwelwch yn dda")),
+      Phrase(Vector("No thanks","Dim Diolch")),
+      Phrase(Vector("Ok coming up","Iawn yn dod i fyny")),
+      Phrase(Vector("Alright","Iawn")),
+      Phrase(Vector("Perhaps you would like something later","Efallai yr hoffech gael rhywbeth yn nes ymlaen"))
+    )
+
+    private val links = Vector( Link(0, "http://food.com,/tea", "Tea", window = false ))
+
+    val process: Process = Process( meta, flow, phrases, links )
+
+    // Define pages created by PageBuilder
+    val page1StanzaSeq = Seq(
+      page1ValueStanza,
+      Instruction( phrases(0), Seq("2"), None, stack = false ),
+      Question( phrases(1), Seq( phrases(2), phrases(3)), Seq(page2Id, page3Id), stack = false )
+    )
+
+    val page1: Page = Page( page1Id, page1Url, page1StanzaSeq, Seq(page2Id, page3Id), Seq.empty )
+
+    val page2StanzaSeq = Seq(
+      page2ValueStanza,
+      Instruction( phrases(four), Seq("end"), Some(links(0)), stack = false),
+      EndStanza
+    )
+
+    val page2: Page = Page( page2Id, page2Url, page2StanzaSeq, Seq.empty, Seq.empty )
+
+    val page3StanzaSeq = Seq(
+      page3ValueStanza,
+      Instruction( phrases(five), Seq( "11" ), None, stack = false ),
+      Instruction( phrases(six), Seq( "end" ), None, stack = false ),
+      EndStanza
+    )
+
+    val page3: Page = Page( page3Id, page3Url, page3StanzaSeq, Seq.empty, Seq.empty )
+
+    val pages: Seq[Page] = Seq( page1, page2, page3 )
 
     val pageBuilder: PageBuilder = new PageBuilder()
     val uiBuilder: UIBuilder = new UIBuilder()
-    lazy val target = new GuidanceService(mockGuidanceConnector, mockSessionRepository, pageBuilder, uiBuilder)
+    //lazy val target = new GuidanceService(mockGuidanceConnector, mockSessionRepository, pageBuilder, uiBuilder)
+    lazy val target = new GuidanceService(mockGuidanceConnector, mockSessionRepository, mockPageBuilder, uiBuilder)
   }
 
   "Calling getPage with a valid URL" should {
@@ -44,6 +123,10 @@ class GuidanceServiceSpec extends BaseSpec {
       MockSessionRepository
         .get(processId)
         .returns(Future.successful(Some(process)))
+
+      MockPageBuilder
+        .pages(process)
+        .returns(Right(pages))
 
       private val result = target.getPage(url, processId)
 
@@ -65,6 +148,10 @@ class GuidanceServiceSpec extends BaseSpec {
         .get(processId)
         .returns(Future.successful(Some(process)))
 
+      MockPageBuilder
+        .pages(process)
+        .returns(Right(pages))
+
       private val result = target.getPage(url, processId)
 
       whenReady(result) { _ mustBe None }
@@ -83,6 +170,10 @@ class GuidanceServiceSpec extends BaseSpec {
       MockSessionRepository
         .set(processId, process)
         .returns(Future.successful(Some(())))
+
+      MockPageBuilder
+        .pages(process)
+        .returns(Right(pages))
 
       private val result = target.getStartPageUrl(processId, processId)
 
