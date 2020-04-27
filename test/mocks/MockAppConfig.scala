e8cb1c4d9a3d208f98eea1161549029aee589d9d
mac grewal <7713555+macgrewal@users.noreply.github.com>
2020-04-27 15:04:40 +0100

    Updated play-frontend-* dependencies and added timeout dialog

--- test/mocks/MockAppConfig.scala
+++ test/mocks/MockAppConfig.scala
@@ -35,5 +35,7 @@ object MockAppConfig extends AppConfig {
   override val privacy: String = "someUrl"
   override val termsConditions: String = "someUrl"
   override val govukHelp: String = "someUrl"
-
+  override val signOutUrl: String = "https://www.gov.uk"
+  override val timeoutInSeconds: Int = 1200
+  override val timeoutWarningInSeconds: Int = 300
 }
