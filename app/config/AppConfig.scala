829e82ed7ef0de5c0264dc9e4e02672f56565639
“RoheelArshad” <48284720+RoheelArshad@users.noreply.github.com>
2020-05-06 11:32:00 +0100

    rebased to EG-411

--- app/config/AppConfig.scala
+++ app/config/AppConfig.scala
@@ -64,10 +64,6 @@ class AppConfigImpl @Inject() (val config: Configuration, servicesConfig: Servic
   def feedbackUrl(implicit request: RequestHeader): String =
     s"$contactBaseUrl/contact/beta-feedback-unauthenticated?service=$serviceIdentifier&backUrl=$requestUri"
 
-  private def requestUri(implicit request: RequestHeader) = ContinueUrl(host + request.uri).encodedUrl
-  def feedbackUrl(implicit request: RequestHeader): String =
-    s"$contactBaseUrl/contact/beta-feedback-unauthenticated?service=$serviceIdentifier&backUrl=$requestUri"
-
   lazy val externalGuidanceBaseUrl: String = servicesConfig.baseUrl("external-guidance")
 
   lazy val cookies: String = config.get[String]("urls.footer.cookies")
