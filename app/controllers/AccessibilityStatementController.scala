7652f89e04dbe5bd8c7c7ce19214c682f96f6dca
R Woollett <55831974+woollett-r@users.noreply.github.com>
2020-09-07 13:10:11 +0100

    Update accessibility controller for process identifier

--- app/controllers/AccessibilityStatementController.scala
+++ app/controllers/AccessibilityStatementController.scala
@@ -46,7 +46,7 @@ class AccessibilityStatementController @Inject() (
     withExistingSession[ProcessContext](service.getProcessContext(_)).map {
       case Right(processContext) =>
         val title = models.ui.Text(processContext.process.title.langs)
-        val processIdentifier = processContext.process.meta.processCode.getOrElse(processContext.process.meta.id)
+        val processIdentifier = processContext.process.getProcessIdentifier()
         Ok(view(title.asString(messages.lang),
                 processContext.process.startUrl.map(url => s"${appConfig.baseUrl}/$processIdentifier$url")))
       case Left(BadRequestError) =>
