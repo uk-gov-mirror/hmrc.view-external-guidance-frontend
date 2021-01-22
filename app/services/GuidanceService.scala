51927929a9b1fa6b0cefcfeb175924eb1fad923e
cmrhmrc <55755517+cmrhmrc@users.noreply.github.com>
2021-01-22 17:36:43 +0000

    EG-1235, added removePassphraseProtection

--- app/services/GuidanceService.scala
+++ app/services/GuidanceService.scala
@@ -28,7 +28,7 @@ import uk.gov.hmrc.http.HeaderCarrier
 import play.api.i18n.Lang
 import scala.concurrent.{ExecutionContext, Future}
 import repositories.{ProcessContext, SessionRepository}
-import core.models.ocelot.stanzas.{ValueStanza, Value, Scalar}
+import core.models.ocelot.stanzas.{Value, ValueStanza}
 import core.models.ocelot.{LabelCache, Labels, Process}
 import core.models.ocelot.SecuredProcess
 
@@ -153,30 +153,29 @@ class GuidanceService @Inject() (
 
   def retrieveAndCacheApproval(processId: String, docId: String)
                               (implicit hc: HeaderCarrier, context: ExecutionContext): Future[RequestOutcome[(String,String)]] = {
-    def retrieveProcessWithAddedPassPhraseResponse(processId: String): Future[RequestOutcome[Process]] =
-      connector.approvalProcess(processId).map{
-        case Right(process) if process.passPhrase.isDefined =>
-          logger.info(s"Attempting to add passphrase response")
-          process
-            .flow
-            .collect{case (k, v: ValueStanza) if v.values.exists(_.label.equals(SecuredProcess.PassPhraseLabelName)) => (k, v)}.headOption
-            .fold[RequestOutcome[Process]](Right(process)){kv =>
-              val (k, vs) = kv
-              val v = Value(Scalar, SecuredProcess.PassPhraseResponseLabelName, process.passPhrase.get)
-              val newVs = vs.copy(values = vs.values :+ v)
-              println(newVs)
-              Right(process.copy(flow = process.flow ++ Seq((k, newVs))))
-          }
-        case res @ Right(_) => res
-        case err @ Left(_) => err
-      }
 
-    retrieveAndCache(processId, docId, retrieveProcessWithAddedPassPhraseResponse)
+    def removedPassphraseProtection(process: Process): Process =
+      // Renames passphrase label and removes the passphrase entry page
+      process.flow.collectFirst{case (k, v: ValueStanza) if v.values.exists(_.label.equals(SecuredProcess.PassPhraseLabelName)) => (k, v)}
+        .fold(process){kv =>
+          val (k, vs) = kv
+          val newVs = vs.copy(values = vs.values.map{
+            case Value(t, SecuredProcess.PassPhraseLabelName, v) => Value(t, s"_${SecuredProcess.PassPhraseLabelName}", v)
+            case v => v
+          })
+          process.copy(flow = process.flow ++ Seq((k, newVs)) -- Seq(SecuredProcess.PassPhrasePageId))
+        }
+
+    // Remove any passphrase protection when viewing or previewing approval processes
+    retrieveAndCache(processId, docId, connector.approvalProcess(_).map{
+      case Right(process) if process.passPhrase.isDefined => Right(removedPassphraseProtection(process))
+      case res @ Right(_) => res
+      case err @ Left(_) => err
+    })
   }
 
-
   private def retrieveAndCache(processIdentifier: String, docId: String, retrieveProcessById: String => Future[RequestOutcome[Process]])(
-      implicit context: ExecutionContext
+    implicit context: ExecutionContext
   ): Future[RequestOutcome[(String,String)]] =
     retrieveProcessById(processIdentifier).flatMap {
       case Left(err) =>
