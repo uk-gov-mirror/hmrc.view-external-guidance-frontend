1a828ba284105016bf14c82621eeefd31efa7105
cmrhmrc <55755517+cmrhmrc@users.noreply.github.com>
2021-03-15 11:21:49 +0000

    EG-1318, removal of old code

--- app/repositories/SessionProcessFSM.scala
+++ app/repositories/SessionProcessFSM.scala
@@ -26,7 +26,7 @@ class SessionProcessFSM @Inject() {
   // Input
   // url ,incoming url
   // priorSp, prior SessionProcess corresponding to last url processed. Note. The db record will have the head of the page history updated
-  //          to include incoming url and lowStack Nil. This update is not within priorSp. This is an optimisation, as the most common transition
+  //          to include incoming url and flowStack Nil, this update is not within priorSp. This is an optimisation, as the most common transition
   //          is forward in a process containing no Sequences (i.e. the flowStack will always be empty), this transition will result in
   //          no page history or flowstack updates given the update described has already taken place.
   // forceForward, true indicates that a url similar to head of prior history (looks like a BACK) should be treated as a forward movement
@@ -35,27 +35,6 @@ class SessionProcessFSM @Inject() {
   // optional backlink to be displayed on page with incoming url
   // optional page history update
   // optional flowStack update
-
-  // private def backlinkAndHistory(pageUrl: String,
-  //                                flowStack: List[FlowStage],
-  //                                previousPageByLink: Boolean,
-  //                                priorHistory: List[PageHistory]): (Option[String], Option[List[PageHistory]], Option[List[FlowStage]]) =
-  //   priorHistory.reverse match {
-  //     // Initial page
-  //     case Nil => (None, None, None)
-  //     // REFRESH: Rewrite pageHistory as current history without current page just added, Back link is x
-  //     case x :: xs if x.url == pageUrl => (xs.headOption.map(_.url), Some(priorHistory), None)
-  //     // BACK: pageHistory becomes y :: xs and backlink is head of xs
-  //     case _ :: y :: xs if y.url == pageUrl && !previousPageByLink => (xs.headOption.map(_.url), Some((y :: xs).reverse), Some(y.flowStack))
-  //     // FORWARD to start url: Back link x, rewrite pageHistory with current flowStack in head, set flowStack to that of initial page in history
-  //     case x :: xs if priorHistory.headOption.fold(false)(h => h.url == pageUrl) =>
-  //       (Some(x.url), Some((PageHistory(pageUrl, flowStack) :: x :: xs).reverse), priorHistory.headOption.map(_.flowStack))
-  //     // FORWARD with a non-empty flowStack: Back link x, rewrite pageHistory with current flowStack in head
-  //     case x :: xs if flowStack.nonEmpty => (Some(x.url), Some((PageHistory(pageUrl, flowStack) :: x :: xs).reverse), None)
-  //     // FORWARD: Back link x, pageHistory intact
-  //     case x :: _ => (Some(x.url), None, None)
-  //   }
-
   def apply(url: String, priorSp: SessionProcess, forceForward: Boolean):  (Option[String], Option[List[PageHistory]], Option[List[FlowStage]]) = {
     println(s"**** FSM url $url, forceForward, $forceForward")
     println(s"**** FSM hist ${priorSp.pageHistory.reverse}")
@@ -82,20 +61,3 @@ class SessionProcessFSM @Inject() {
   }
 }
 
-      // case Nil => (None, None, None, None) // Initial Page
-      // // REFRESH: Rewrite pageHistory as current history without current page just added, this is conditional on there being no change to the FlowStack
-      // case x :: xs if x.url == url => (xs.headOption.map(_.url), Some(priorSp.pageHistory), None, None)
-
-      // // BACK: pageHistory becomes y :: xs and backlink is head of xs, conditional on forward == false
-      // case _ :: y :: xs if y.url == url && !forceForward =>
-      //   val labelUpdate: Option[List[ScalarLabel]] = y.flowStack.reverse.headOption.collect{
-      //     case f: Flow if f.labelValue.nonEmpty && f.labelValue.get.value.nonEmpty => List(ScalarLabel(f.labelValue.get.name, List(f.labelValue.get.value.get)))
-      //   }
-      //   println(s"*************** $labelUpdate")
-      //   (xs.headOption.map(_.url), Some((y :: xs).reverse), Some(y.flowStack), labelUpdate)
-
-      // // FORWARD with a non-empty flowStack: Back link x, rewrite pageHistory with current flowStack in head
-      // case x :: xs if priorSp.flowStack.nonEmpty => (Some(x.url), Some((PageHistory(url, priorSp.flowStack) :: x :: xs).reverse), None, None)
-
-      // // FORWARD: Back link x, pageHistory intact
-      // case x :: _ => (Some(x.url), None, None, None)
