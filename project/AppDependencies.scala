84f81ef27b0988c63010734bab492375c9c33a53
Jayasree Atmakuru <7058921+jayasreemca@users.noreply.github.com>
2026-08-18 10:42:02 +0100

    DL-20042: Update libs. (#498)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -18,13 +18,13 @@ import sbt.*
 
 object AppDependencies {
 
-  val bootstrapVersion = "10.7.0"
-  val hmrcMongoVersion = "2.12.0"
+  val bootstrapVersion = "10.8.0"
+  val hmrcMongoVersion = "2.13.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"         % hmrcMongoVersion,
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "12.32.1"
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "13.11.0"
   )
 
   val test: Seq[ModuleID] = Seq(
